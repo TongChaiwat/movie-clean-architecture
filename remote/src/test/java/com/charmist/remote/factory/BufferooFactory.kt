f53527674cb16f614665870608d6fcd0b358477a
Chaiwat Luecha <chaiwat.lue@mtel.co.th>
2020-10-27 16:46:37 +0700

    add test remote

---
+++ remote/src/test/java/com/charmist/remote/factory/BufferooFactory.kt
@@ -0,0 +1,32 @@
+package com.charmist.remote.factory
+
+import com.charmist.remote.BufferooService
+import com.charmist.remote.factory.DataFactory.Factory.randomUuid
+import com.charmist.remote.model.BufferooModel
+
+
+class BufferooFactory {
+
+    companion object Factory {
+
+        fun makeBufferooResponse(): BufferooService.BufferooResponse {
+            val bufferooResponse = BufferooService.BufferooResponse()
+            bufferooResponse.team = makeBufferooModelList(5)
+            return bufferooResponse
+        }
+
+        fun makeBufferooModelList(count: Int): List<BufferooModel> {
+            val bufferooEntities = mutableListOf<BufferooModel>()
+            repeat(count) {
+                bufferooEntities.add(makeBufferooModel())
+            }
+            return bufferooEntities
+        }
+
+        fun makeBufferooModel(): BufferooModel {
+            return BufferooModel(randomUuid(), randomUuid(), randomUuid())
+        }
+
+    }
+
+}
