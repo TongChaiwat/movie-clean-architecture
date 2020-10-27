413025fd4f4bc21740b312d762eff037555dfcf0
Chaiwat Luecha <chaiwat.lue@mtel.co.th>
2020-10-27 16:51:34 +0700

    add test cache

---
+++ cache/src/test/java/com/charmist/cache/BufferooCacheImplTest.kt
@@ -0,0 +1,103 @@
+package com.charmist.cache
+
+import com.charmist.cache.db.Db
+import com.charmist.cache.db.DbOpenHelper
+import com.charmist.cache.db.mapper.BufferooMapper
+import com.charmist.cache.factory.BufferooFactory
+import com.charmist.cache.mapper.BufferooEntityMapper
+import org.buffer.android.boilerplate.cache.PreferencesHelper
+import org.buffer.android.boilerplate.cache.model.CachedBufferoo
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21])
+class BufferooCacheImplTest {
+
+    private var entityMapper = BufferooEntityMapper()
+    private var mapper = BufferooMapper()
+    private var preferencesHelper = PreferencesHelper(RuntimeEnvironment.application)
+
+    private val databaseHelper = BufferooCacheImpl(
+        DbOpenHelper(RuntimeEnvironment.application),
+        entityMapper, mapper, preferencesHelper
+    )
+
+    @Before
+    fun setup() {
+        databaseHelper.getDatabase().rawQuery("DELETE FROM " + Db.BufferooTable.TABLE_NAME, null)
+    }
+
+    @Test
+    fun clearTablesCompletes() {
+        val testObserver = databaseHelper.clearBufferoos().test()
+        testObserver.assertComplete()
+    }
+
+    //<editor-fold desc="Save Bufferoos">
+    @Test
+    fun saveBufferoosCompletes() {
+        val bufferooEntities = BufferooFactory.makeBufferooEntityList(2)
+
+        val testObserver = databaseHelper.saveBufferoos(bufferooEntities).test()
+        testObserver.assertComplete()
+    }
+
+    @Test
+    fun saveBufferoosSavesData() {
+        val bufferooCount = 2
+        val bufferooEntities = BufferooFactory.makeBufferooEntityList(bufferooCount)
+
+        databaseHelper.saveBufferoos(bufferooEntities).test()
+        checkNumRowsInBufferoosTable(bufferooCount)
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Get Bufferoos">
+    @Test
+    fun getBufferoosCompletes() {
+        val testObserver = databaseHelper.getBufferoos().test()
+        testObserver.assertComplete()
+    }
+
+    @Test
+    fun getBufferoosReturnsData() {
+        val bufferooEntities = BufferooFactory.makeBufferooEntityList(2)
+        val cachedBufferoos = mutableListOf<CachedBufferoo>()
+        bufferooEntities.forEach {
+            cachedBufferoos.add(entityMapper.mapToCached(it))
+        }
+        insertBufferoos(cachedBufferoos)
+
+        val testObserver = databaseHelper.getBufferoos().test()
+        testObserver.assertValue(bufferooEntities)
+    }
+    //</editor-fold>
+
+    private fun insertBufferoos(cachedBufferoos: List<CachedBufferoo>) {
+        val database = databaseHelper.getDatabase()
+        cachedBufferoos.forEach {
+            database.insertOrThrow(
+                Db.BufferooTable.TABLE_NAME, null,
+                mapper.toContentValues(it)
+            )
+        }
+    }
+
+    private fun checkNumRowsInBufferoosTable(expectedRows: Int) {
+        val bufferoosCursor = databaseHelper.getDatabase().query(
+            Db.BufferooTable.TABLE_NAME,
+            null, null, null, null, null, null
+        )
+        bufferoosCursor.moveToFirst()
+        val numberOfRows = bufferoosCursor.count
+        bufferoosCursor.close()
+        assertEquals(expectedRows, numberOfRows)
+    }
+
+}
---
+++ cache/src/test/java/com/charmist/cache/factory/BufferooFactory.kt
@@ -0,0 +1,37 @@
+package com.charmist.cache.factory
+
+import com.charmist.data.model.BufferooEntity
+import org.buffer.android.boilerplate.cache.model.CachedBufferoo
+import com.charmist.cache.factory.DataFactory.Factory.randomUuid
+
+class BufferooFactory {
+
+    companion object Factory {
+
+        fun makeCachedBufferoo(): CachedBufferoo {
+            return CachedBufferoo(randomUuid(), randomUuid(), randomUuid())
+        }
+
+        fun makeBufferooEntity(): BufferooEntity {
+            return BufferooEntity(randomUuid(), randomUuid(), randomUuid())
+        }
+
+        fun makeBufferooEntityList(count: Int): List<BufferooEntity> {
+            val bufferooEntities = mutableListOf<BufferooEntity>()
+            repeat(count) {
+                bufferooEntities.add(makeBufferooEntity())
+            }
+            return bufferooEntities
+        }
+
+        fun makeCachedBufferooList(count: Int): List<CachedBufferoo> {
+            val cachedBufferoos = mutableListOf<CachedBufferoo>()
+            repeat(count) {
+                cachedBufferoos.add(makeCachedBufferoo())
+            }
+            return cachedBufferoos
+        }
+
+    }
+
+}
